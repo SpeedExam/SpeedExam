5d2a6e18d19bc6a2b80368a174a5fe12fb16eb91
Bilal El Maftouhi <maftouhi.bilal@yahoo.com>
2023-12-15 11:09:40 +0100

    init_v3_groupid_artifactid

--- server/SpeedExam/src/test/java/com/ensat/speedexam/SpeedExamApplicationTests.java
+++ server/SpeedExam/src/test/java/com/ensat/speedexam/SpeedExamApplicationTests.java
@@ -1,10 +1,10 @@
-package com.speedexam.speedex;
+package com.ensat.speedexam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SpeedExamApplicationTests {
 
 	@Test
 	void contextLoads() {
