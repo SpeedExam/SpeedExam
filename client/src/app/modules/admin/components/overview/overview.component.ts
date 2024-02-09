37316025a416765ac879ca21a50de04648211804
oussamazaoui <133207079+oussamazaoui@users.noreply.github.com>
2024-02-09 01:01:23 +0100

    last version

--- client/src/app/modules/admin/components/overview/overview.component.ts
+++ client/src/app/modules/admin/components/overview/overview.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {ProfService} from "../../../../services/profService/prof.service";
+import {NgToastService} from "ng-angular-popup";
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
-export class OverviewComponent {
-  constructor(private route:Router) {
+export class OverviewComponent implements OnInit{
+  constructor(private route:Router,private srv:ProfService,private toast: NgToastService ) {
   }
 onClick(){
     this.route.navigate(["/prof"]);
 }
+  subjects:any[]=[];
+  empty:boolean =false;
+
+  ngOnInit(): void {
+    this.getsSubjects();
+  }
+  getsSubjects(){
+    this.srv.getallExams().subscribe(
+      (res:any)=>{
+        this.subjects=res;
+        this.empty = this.subjects.length != 0;
+        console.log(this.empty);
+      }
+    )
+
+  }
+
+  delete(index: number) {
+
+    let id = this.subjects[index].id;
+    this.subjects.splice(index,1);
+    this.srv.deleteSubject(id).subscribe(
+      res=>{
+        this.toast.warning({detail:"Deletion",summary:'The Exam is deleted',sticky:true});
+      }
+    )
+
+
+  }
+
 }
